df29a8ec74a58e49a8988baa51d0d150b3d54257
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-09 18:48:57 +0600

    start project

---
+++ src/main/java/kz/yessenali/ecommerce/SpringBootEcommerceApplication.java
@@ -0,0 +1,13 @@
+package kz.yessenali.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+	}
+
+}
