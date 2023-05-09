4e9774f5b789ac2d23bd1a3a6d7e9979a9a45022
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-09 19:02:28 +0600

    add annotations, column name for fields

--- src/main/java/kz/yessenali/ecommerce/entity/Product.java
+++ src/main/java/kz/yessenali/ecommerce/entity/Product.java
@@ -1,8 +1,9 @@
 package kz.yessenali.ecommerce.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -11,23 +12,38 @@ import java.util.Date;
 @Table(name="product")
 @Data
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
     private Long id;
 
+    @Column(name="sku")
     private String sku;
 
+    @Column(name="name")
     private String name;
 
+    @Column(name="description")
     private String description;
 
+    @Column(name="unit_price")
     private BigDecimal unitPrice;
 
+    @Column(name="image_url")
     private String imageUrl;
 
+    @Column(name="active")
     private boolean active;
 
+    @Column(name="units_in_stock")
     private int unitInStock;
 
+    @Column(name="date_created")
+    @CreationTimestamp
     private Date dateCreated;
 
+    @Column(name="last_updated")
+    @UpdateTimestamp
     private Date lastUpdated;
 }
