012f6627c9f7fae27aa6ebdcb8cb19ed1ccdd333
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-09 19:54:45 +0600

    add MyDataRestConfig.java file in order to REST APIs Configure for Read Only

--- src/main/java/kz/yessenali/ecommerce/SpringBootEcommerceApplication.java
+++ src/main/java/kz/yessenali/ecommerce/SpringBootEcommerceApplication.java
@@ -3,6 +3,7 @@ package kz.yessenali.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringBootEcommerceApplication {
 
