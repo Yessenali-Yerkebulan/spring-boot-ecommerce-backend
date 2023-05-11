f43473c3d11ecaa54908fee20fa464c9b8e4d695
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-11 19:11:36 +0600

    add CrossOrigin annotation to ProductCategoryRepository and ProductRepository

--- src/main/java/kz/yessenali/ecommerce/dao/ProductRepository.java
+++ src/main/java/kz/yessenali/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,9 @@ package kz.yessenali.ecommerce.dao;
 
 import kz.yessenali.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
