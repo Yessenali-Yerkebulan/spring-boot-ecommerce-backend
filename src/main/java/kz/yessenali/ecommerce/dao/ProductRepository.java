f94a5f07bc21725f925adc6cc3902f2c8d351185
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-16 19:38:44 +0600

    add findByCategoryId method to ProductRepository

--- src/main/java/kz/yessenali/ecommerce/dao/ProductRepository.java
+++ src/main/java/kz/yessenali/ecommerce/dao/ProductRepository.java
@@ -1,10 +1,13 @@
 package kz.yessenali.ecommerce.dao;
 
 import kz.yessenali.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
