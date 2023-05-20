536a290162c087463fa31a4fa85302f1fecc6e85
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-05-20 18:26:09 +0600

    add findByNameContaining for search products by name

--- src/main/java/kz/yessenali/ecommerce/dao/ProductRepository.java
+++ src/main/java/kz/yessenali/ecommerce/dao/ProductRepository.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
 }
