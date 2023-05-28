6f330b07235511ece9999c5763b89b73cbceda46
Yerkebulan <yerkebulanyessenali03@gmail.com>
2023-05-28 09:28:33 +0600

    create Database entities for checkout form save the order

---
+++ src/main/java/kz/yessenali/ecommerce/entity/Customer.java
@@ -0,0 +1,25 @@
+package kz.yessenali.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="customer")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+}
